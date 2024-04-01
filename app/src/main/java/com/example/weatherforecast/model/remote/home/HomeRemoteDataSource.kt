b133e66c145a0517106efa2fef696c8f817b9d7c
minaemad6060@gmail.com <minaemad6060@gmail.com>
2024-04-01 06:09:39 +0200

    fix bug in Map Activity

--- app/src/main/java/com/example/weatherforecast/model/remote/home/HomeRemoteDataSource.kt
+++ app/src/main/java/com/example/weatherforecast/model/remote/home/HomeRemoteDataSource.kt
@@ -1,15 +1,11 @@
 package com.example.weatherforecast.Model.Remote.Home
 
-
 import com.example.weatherforecast.model.Remote.ApiService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-
-
 class HomeRemoteDataSource(private val homeAPI: ApiService): InterRemoteDataSource
 {
-
     override suspend fun getAdditionalWeatherRemote(
         lat: Double, lon: Double, key: String, units: String, lang: String, cnt: Int
     ): Flow<CurrentWeather> = flow {
