5d5fb173cbd056e2e14a483391d556481ad3a5f7
minaemad6060@gmail.com <minaemad6060@gmail.com>
2024-04-12 07:12:18 +0200

    edit in alert

--- app/src/main/java/com/example/weatherforecast/Model/Local/Fav/InterFavLocalDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.example.weatherforecast.Model.Local.Fav
-
-import kotlinx.coroutines.flow.Flow
-
-interface InterFavLocalDataSource {
-    fun getFavWeatherLocal(): Flow<List<FavWeather>>
-    suspend fun deleteFavWeatherLocal(favWeather: FavWeather): Int
-    suspend fun insertFavWeatherLocal(favWeather: FavWeather): Long
-}
