a3be10a80db1da996f2f4577f911e65c1e284070
minaemad6060@gmail.com <minaemad6060@gmail.com>
2024-04-12 05:19:46 +0200

    edit in InterAlertRepo

--- app/src/main/java/com/example/weatherforecast/model/repo/alert/InterAlertRepo.kt
+++ app/src/main/java/com/example/weatherforecast/model/repo/alert/InterAlertRepo.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 
 interface InterAlertRepo {
+    suspend fun getAlertWeatherRemoteRepo(
+        lat: Double, lon: Double, key: String
+    ): Flow<OneCallAlert>
     suspend fun getAlertWeatherLocalRepo(): Flow<List<AlertCalendar>>
     suspend fun deleteAlertWeatherLocalRepo(id: String): Int
     suspend fun insertAlertWeatherLocalRepo(alertCalendar: AlertCalendar): Long
