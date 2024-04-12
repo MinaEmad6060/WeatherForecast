3433a8853f5346e09096302a1f20eb089ae43237
minaemad6060@gmail.com <minaemad6060@gmail.com>
2024-04-12 05:58:47 +0200

    edit in alert repo file

--- app/src/main/java/com/example/weatherforecast/model/repo/alert/InterAlertRepo.kt
+++ app/src/main/java/com/example/weatherforecast/model/repo/alert/InterAlertRepo.kt
@@ -3,6 +3,7 @@ package com.example.weatherforecast.Model.Repo.Alert
 import com.example.weatherforecast.Model.Local.Alert.AlertCalendar
 import kotlinx.coroutines.flow.Flow
 
+
 interface InterAlertRepo {
     suspend fun getAlertWeatherLocalRepo(): Flow<List<AlertCalendar>>
     suspend fun deleteAlertWeatherLocalRepo(id: String): Int
