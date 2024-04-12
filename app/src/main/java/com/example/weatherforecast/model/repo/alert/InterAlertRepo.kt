35483cc62d891a003f8e845610cb0f7a5083fc26
minaemad6060@gmail.com <minaemad6060@gmail.com>
2024-04-12 02:42:37 +0200

    edit in Interface AlertRepo fragment

--- app/src/main/java/com/example/weatherforecast/model/repo/alert/InterAlertRepo.kt
+++ app/src/main/java/com/example/weatherforecast/model/repo/alert/InterAlertRepo.kt
@@ -1,13 +1,15 @@
 package com.example.weatherforecast.Model.Repo.Alert
 
-
+import android.content.Context
 import com.example.weatherforecast.Model.Local.Alert.AlertCalendar
-
+import com.example.weatherforecast.Model.Remote.Alert.OneCallAlert
 import kotlinx.coroutines.flow.Flow
-
+import kotlinx.coroutines.flow.StateFlow
 
 interface InterAlertRepo {
-
+    suspend fun getAlertWeatherRemoteRepo(
+        lat: Double, lon: Double, key: String
+    ): Flow<OneCallAlert>
     suspend fun getAlertWeatherLocalRepo(): Flow<List<AlertCalendar>>
     suspend fun deleteAlertWeatherLocalRepo(id: String): Int
     suspend fun insertAlertWeatherLocalRepo(alertCalendar: AlertCalendar): Long
